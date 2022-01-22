9521c26e8024cccab29da613b9332b3ef021d70a
Megan Campbell <megancampbell201@gmail.com>
2022-01-22 17:26:46 -0500

    created a function to change class of time block depending on hour and edited HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,29 @@
 
+
+// displays current day month and year at top of page
 var currentDay = moment(new Date()).format("MM/DD/YYYY");
 $("#currentDay").text(currentDay);
 
+function timeBlockColor() {
+    //current hour
+    var hour = moment().hours();
 
-  
+    // change color of time block depending on hour
+    $(".time-block").each(function() {
+        // control color of each individual time block by using id
+        var currentHour = parseInt($(this).attr("id"));
+
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+// call the function 
+timeBlockColor();
+
